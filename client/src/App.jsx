2199fee1f6698bc533c3114133b56bd07ce2e3fb
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-04-30 23:38:34 +0530

    Install react js and tailwind css create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+    console.log("Hi");
+    console.log("Hnjdn");
+    return (
+        <>
+            <div className="text-red-500 font-extrabold">App</div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
