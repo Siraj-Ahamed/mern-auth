a5e325976acb0a50239aad57b9ae3567a414f55d
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-05-04 12:06:05 +0530

    Add middleware and function to jhandle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -9,15 +9,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        // res.status(500).json(error.message);
-        if (error.code === 11000 && error.keyPattern.username) {
-            // Duplicate username error
-            res.status(400).json({
-                error: "Username already exists. Please choose a different username.",
-            });
-        } else {
-            // Other errors
-            res.status(500).json({ error: "Internal server error" });
-        }
+        next(error);
     }
 };
