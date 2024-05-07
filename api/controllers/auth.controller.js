ee72210babc2c640d571d7fc3c2dc680b2ae398d
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-05-07 20:39:19 +0530

    complete sign in page fuctionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -33,13 +33,15 @@ export const signin = async (req, res, next) => {
 
         // Generate token
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-        const expiryDate = new Date(Date.now()) + 3600000; // 1 hour
-        res.cookie("access_token", token, {
-            httpOnly: true,
-            expires: expiryDate,
-        })
-            .status(200)
-            .json(rest);
+        // const expiryDate = new Date(Date.now()) + 3600000; // 1 hour
+        // res.cookie("access_token", token, {
+        //     httpOnly: true,
+        //     expires: expiryDate,
+        // })
+        //     .status(200)
+        //     .json(rest);
+
+        res.status(200).json(rest);
     } catch (error) {
         next(error);
     }
